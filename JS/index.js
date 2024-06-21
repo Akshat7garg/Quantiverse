f5dc943b05534536530a038f997ea2166d6d2934
Akshat7garg <akshat7garg@gmail.com>
2024-06-21 13:20:53 +0530

    Recreating whole webpage

--- JS/index.js
+++
@@ -1,8 +0,0 @@
-// Logic building for the drop-down hamburger option
-let btn = document.getElementById('menu-btn');
-let dropBox = document.getElementById('drop');
-
-btn.addEventListener("click", ()=> {
-    btn.classList.toggle('hamburger');
-    dropBox.classList.toggle('drop-box');
-});
